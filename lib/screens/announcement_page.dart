fd4c03206d586b2e6a2788cf08a36908872a8d0a
Uzay <uzayyurdugur@gmail.com>
2024-06-12 20:27:32 +0300

    .

--- lib/screens/announcement_page.dart
+++ lib/screens/announcement_page.dart
@@ -10,21 +10,33 @@ import 'package:tobetoapp/screens/add_announcement.dart';
 import 'package:tobetoapp/screens/announcement_detail.dart';
 
 
-class AnnouncementsPage extends StatelessWidget {
+class AnnouncementsPage extends StatefulWidget {
   final UserRole? role;
   final List<String>? classIds;
 
   const AnnouncementsPage({super.key, required this.role, this.classIds});
 
   @override
-  Widget build(BuildContext context) {
-    // Duyurularin sinifa ve role gore on yuklenme durumu
-    context.read<AnnouncementBloc>().add(LoadAnnouncements(classIds, role));
+  State<AnnouncementsPage> createState() => _AnnouncementsPageState();
+}
 
+class _AnnouncementsPageState extends State<AnnouncementsPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Duyurularin sinifa ve role gore on yuklenme durumu
+    context
+        .read<AnnouncementBloc>()
+        .add(LoadAnnouncements(widget.classIds, widget.role));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Announcements'),
-        actions: role == UserRole.admin
+        title: const Text('Tobeto'),
+        centerTitle: true,
+        actions: widget.role == UserRole.admin
             ? [
                 IconButton(
                   icon: const Icon(Icons.add),
@@ -32,7 +44,7 @@ class AnnouncementsPage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const AddAnnouncementPage(),
+                        builder: (context) => AddAnnouncementPage(),
                       ),
                     );
                   },
@@ -40,118 +52,153 @@ class AnnouncementsPage extends StatelessWidget {
               ]
             : null,
       ),
-      body: BlocConsumer<AnnouncementBloc, AnnouncementState>(
-        listener: (context, state) {
-          if (state is AnnouncementOperationSuccess) {
-            ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Operation Successful')));
-          } else if (state is AnnouncementOperationFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Operation Failed')));
-          }
-        },
-        builder: (context, state) {
-          if (state is AnnouncementsLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is AnnouncementsLoaded) {
-            if (state.announcements.isEmpty) {
-              return const Center(child: Text('No announcements available.'));
-            } else {
-              final reversedAnnouncements =
-                  state.announcements.reversed.toList();
-              return ListView.builder(
-                itemCount: reversedAnnouncements.length,
-                itemBuilder: (context, index) {
-                  final announcement = reversedAnnouncements[index];
-                  final formattedDate = announcement.createdAt != null
-                      ? DateFormat('dd.MM.yyyy').format(announcement.createdAt!)
-                      : 'No date';
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+      body: Column(
+        children: [
+          // Sabit Banner
+          SizedBox(
+            width: double.infinity,
+            height: 150, // Banner yüksekliği
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/lessons_banner.png', // Banner resmi
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Align(          
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0), 
+                    child: Text(
+                      "Duyurularım", // Banner içindeki yazı
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AnnouncementDetailPage(
-                              announcement: announcement,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Duyurular Listesi
+          Expanded(
+            child: BlocConsumer<AnnouncementBloc, AnnouncementState>(
+              listener: (context, state) {
+                if (state is AnnouncementOperationSuccess) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Operation Successful')));
+                } else if (state is AnnouncementOperationFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Operation Failed')));
+                }
+              },
+              builder: (context, state) {
+                if (state is AnnouncementsLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is AnnouncementsLoaded) {
+                  if (state.announcements.isEmpty) {
+                    return const Center(child: Text('No announcements available.'));
+                  } else {
+                    final reversedAnnouncements =
+                        state.announcements.reversed.toList();
+                    return ListView.builder(
+                      itemCount: reversedAnnouncements.length,
+                      itemBuilder: (context, index) {
+                        final announcement = reversedAnnouncements[index];
+                        final formattedDate = announcement.createdAt != null
+                            ? DateFormat('dd.MM.yyyy').format(announcement.createdAt!)
+                            : 'No date';
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AnnouncementDetailPage(
+                                    announcement: announcement,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Announcement',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Text(
+                                    announcement.title!,
+                                    style: const TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          const Icon(Icons.calendar_today, size: 16.0),
+                                          const SizedBox(width: 4.0),
+                                          Text(formattedDate),
+                                        ],
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  AnnouncementDetailPage(
+                                                announcement: announcement,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        child: const Text('Details'),
+                                      ),
+                                      if (widget.role == UserRole.admin)
+                                        IconButton(
+                                          icon: const Icon(Icons.delete),
+                                          onPressed: () {
+                                            context.read<AnnouncementBloc>().add(
+                                                DeleteAnnouncement(widget.classIds,
+                                                    announcement.id!, widget.role!));
+                                          },
+                                        ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         );
                       },
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Announcement',
-                              style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Text(
-                              announcement.title!,
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    const Icon(Icons.calendar_today,
-                                        size: 16.0),
-                                    const SizedBox(width: 4.0),
-                                    Text(formattedDate),
-                                  ],
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            AnnouncementDetailPage(
-                                          announcement: announcement,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  child: const Text('Details'),
-                                ),
-                                if (role == UserRole.admin)
-                                  IconButton(
-                                    icon: const Icon(Icons.delete),
-                                    onPressed: () {
-                                      context.read<AnnouncementBloc>().add(
-                                          DeleteAnnouncement(classIds,
-                                              announcement.id!, role!));
-                                    },
-                                  ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            }
-          } else if (state is AnnouncementOperationFailure) {
-            return const Center(child: Text('Failed to load announcements'));
-          } else {
-            return const Center(child: Text('Failed to load announcements'));
-          }
-        },
+                    );
+                  }
+                } else if (state is AnnouncementOperationFailure) {
+                  return const Center(child: Text('Failed to load announcements'));
+                } else {
+                  return const Center(child: Text('Failed to load announcements'));
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/favorites_page.dart
+++ lib/screens/favorites_page.dart
@@ -23,8 +23,6 @@ class _FavoritesPageState extends State<FavoritesPage> {
         LoadFavorites()); // Uygulama baslatildiginda favorilere eklenmis dersin gosterilmesini sagliyoruz.
   }
 
-  //BlocConsumer kullan!
-
   @override
   Widget build(BuildContext context) {
     final lessonRepository = context.read<LessonRepository>();
@@ -33,85 +31,119 @@ class _FavoritesPageState extends State<FavoritesPage> {
       appBar: AppBar(
         title: Text('Favorites'),
       ),
-      body: BlocListener<FavoritesBloc, FavoritesState>(
-        listener: (context, state) {
-          if (state is FavoritesError) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Error: ${state.message}')),
-            );
-          }
-        },
-        child: BlocBuilder<FavoritesBloc, FavoritesState>(
-          builder: (context, state) {
-            if (state is FavoritesLoading) {
-              return Center(child: CircularProgressIndicator());
-            } else if (state is FavoritesLoaded) {
-              final favoriteLessonIds = state.favoriteLessonIds;
-              if (favoriteLessonIds.isEmpty) {
-                return Center(child: Text('Henuz favorılere ders eklenmemistir!'));
-              } else {
-                return ListView.builder(
-                  itemCount: favoriteLessonIds.length,
-                  itemBuilder: (context, index) {
-                    final lessonId = favoriteLessonIds[index];
-               
-                    return FutureBuilder<LessonModel>(
-                      future: lessonRepository.getLessonById(lessonId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(child: CircularProgressIndicator());
-                        } else if (snapshot.hasError) {
-                          print(
-                              'Dersleri yuklerken hata olustu $lessonId: ${snapshot.error}');
-                          return ListTile(
-                            title: Text('Error: ${snapshot.error}'),
-                          );
-                        } else if (snapshot.hasData) {
-                          final lesson = snapshot.data!;
-                          return ListTile(
-                            leading: lesson.image != null
-                                ? Image.network(lesson.image!)
-                                : null,
-                            title: Text(lesson.title ?? 'No title'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(lesson.description ?? 'No description'),
-                                Text(
-                                  DateFormat('dd MMM yyyy, HH:mm')
-                                      .format(lesson.startDate!),
-                                ),
-                              ],
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      LessonDetailsPage(lesson: lesson),
-                                ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 150, // Banner yüksekliği
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/logo/general_banner.png', // Banner resmi
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "Favorilerim", // Banner içindeki yazı
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: BlocConsumer<FavoritesBloc, FavoritesState>(
+              listener: (context, state) {
+                if (state is FavoritesError) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error: ${state.message}')),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is FavoritesLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is FavoritesLoaded) {
+                  final favoriteLessonIds = state.favoriteLessonIds;
+                  if (favoriteLessonIds.isEmpty) {
+                    return Center(
+                        child: Text('Henuz favorılere ders eklenmemistir!'));
+                  } else {
+                    return ListView.builder(
+                      itemCount: favoriteLessonIds.length,
+                      itemBuilder: (context, index) {
+                        final lessonId = favoriteLessonIds[index];
+
+                        return FutureBuilder<LessonModel>(
+                          future: lessonRepository.getLessonById(lessonId),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              print(
+                                  'Dersleri yuklerken hata olustu $lessonId: ${snapshot.error}');
+                              return ListTile(
+                                title: Text('Error: ${snapshot.error}'),
                               );
-                            },
-                          );
-                        } else {
-                          print('ID ait ders bulunamamistir: $lessonId');
-                          return ListTile(
-                            title: Text('Ders bulunamadi'),
-                          );
-                        }
+                            } else if (snapshot.hasData) {
+                              final lesson = snapshot.data!;
+                              return ListTile(
+                                leading: lesson.image != null
+                                    ? Image.network(lesson.image!)
+                                    : null,
+                                title: Text(lesson.title ?? 'No title'),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                        lesson.description ?? 'No description'),
+                                    Text(
+                                      DateFormat('dd MMM yyyy, HH:mm')
+                                          .format(lesson.startDate!),
+                                    ),
+                                  ],
+                                ),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          LessonDetailsPage(lesson: lesson),
+                                    ),
+                                  );
+                                },
+                              );
+                            } else {
+                              print('ID ait ders bulunamamistir: $lessonId');
+                              return ListTile(
+                                title: Text('Ders bulunamadi'),
+                              );
+                            }
+                          },
+                        );
                       },
                     );
-                  },
-                );
-              }
-            } else if (state is FavoritesError) {
-              return Center(child: Text('Error: ${state.message}'));
-            } else {
-              return Center(child: Text('No favorites added'));
-            }
-          },
-        ),
+                  }
+                } else if (state is FavoritesError) {
+                  return Center(child: Text('Error: ${state.message}'));
+                } else {
+                  return Center(child: Text('No favorites added'));
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
